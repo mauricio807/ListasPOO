7982c4bc06f9f5322835ee3778b768c3a69e437a
Luan-Amor <37778171+Luan-Amor@users.noreply.github.com>
2018-08-31 12:38:18 -0300

    Update Principal.java

--- Lista04/Principal.java
+++ Lista04/Principal.java
@@ -14,10 +14,22 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String p[] = new String[3];
+    CartaoWeb c[] = new CartaoWeb[3];
         
-        for (int i = 0; i < p.length; i++){
-            
+        c[0] = new Natal("Nathalia");
+        c[1] = new Aniversario("Julia");
+        c[2] = new DiaDosNamorados("Andressa");
+        
+        String nome[] = new String[3];
+
+        for(int i = 0; i < c.length; i++){
+            nome[0] = JOptionPane.showInputDialog("Qual o seu nome?", "Digite aqui");
+            JOptionPane.showMessageDialog(null,c[0].retornaMensagem(nome[0]));
+            nome[1] = JOptionPane.showInputDialog("Qual o seu nome?", "Digite aqui");
+            JOptionPane.showMessageDialog(null,c[1].retornaMensagem(nome[1]));
+            nome[2] = JOptionPane.showInputDialog("Qual o seu nome?", "Digite aqui");
+            JOptionPane.showMessageDialog(null,c[2].retornaMensagem(nome[2]));
         }
     }
+    
 }
